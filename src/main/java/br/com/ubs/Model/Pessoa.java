87cff0f6f257e1dd3c21bb6c8f92a62256dd693a
Samanta Jacques <samantajacques@gmail.com>
2014-07-30 23:48:01 -0300

    Populei a Package Model e anotei a classe Pessoa

--- src/main/java/br/com/ubs/Model/Pessoa.java
+++ src/main/java/br/com/ubs/Model/Pessoa.java
@@ -1,7 +1,18 @@
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Pessoa {
  
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int codigo;
 	 
 	private String cpf;
